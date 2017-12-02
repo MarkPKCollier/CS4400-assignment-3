3c707951d15b5f1cb40249ccc3775ae4ca335e62
Mark Collier <markcollier@cs.uchicago.edu>
2017-12-02 00:33:45 +0000

    api implementation structure done, need to implement helper methods

--- distributed_file_system/api.py
+++ distributed_file_system/api.py
@@ -7,9 +7,22 @@ from git import Repo
 
 app = Flask(__name__)
 
-# open {r, w}, close, read, write
-
 def does_file_exist(file_id):
+    pass
+
+def open_(file_id, mode):
+    pass
+
+def close_(file_id):
+    pass
+
+def read_(file_id, num_bytes):
+    pass
+
+def write_(file_id, bytes):
+    pass
+
+def delete_(file_id):
     pass
 
 @app.route("/", methods=['GET', 'POST', 'PUT', 'DELETE'])
@@ -24,6 +37,17 @@ def api():
                     'status': 'error',
                     'error_message': 'File ID: {0} does not exist'.format(file_id)
                 })
+            else:
+                try:
+                    write_(file_id, bytes)
+                    return jsonify({
+                        'status': 'success'
+                    })
+                except Exception as e:
+                    return jsonify({
+                        'status': 'error',
+                        'error_message': e
+                    })
         else:
             return jsonify({
                 'status': 'error',
@@ -44,16 +68,33 @@ def api():
                             'status': 'error',
                             'error_message': 'File ID: {0} does not exist'.format(file_id)
                         })
-                    else:
-                        pass
-                elif mode == 'write':
-                    pass
+                try:
+                    open_(file_id, mode)
+                    return jsonify({
+                        'status': 'success'
+                    })
+                except Exception as e:
+                    return jsonify({
+                        'status': 'error',
+                        'error_message': e
+                    })
         elif operation == 'close':
             if not does_file_exist(file_id):
                 return jsonify({
                     'status': 'error',
                     'error_message': 'File ID: {0} does not exist'.format(file_id)
                 })
+            else:
+                try:
+                    close_(file_id)
+                    return jsonify({
+                        'status': 'success'
+                    })
+                except Exception as e:
+                    return jsonify({
+                        'status': 'error',
+                        'error_message': e
+                    })
         else:
             return jsonify({
                 'status': 'error',
@@ -67,12 +108,24 @@ def api():
                     'error_message': 'File ID: {0} does not exist'.format(file_id)
                 })
 
-            num_bytes = request.args.get('num_bytes')
-            if not num_bytes:
-                return jsonify({
-                    'status': 'error',
-                    'error_message': 'You called the read operation, but did not specify num_bytes'
-                })
+            else:
+                num_bytes = request.args.get('num_bytes')
+                if not num_bytes:
+                    return jsonify({
+                        'status': 'error',
+                        'error_message': 'You called the read operation, but did not specify num_bytes'
+                    })
+                else:
+                    try:
+                        read_(file_id, num_bytes)
+                        return jsonify({
+                            'status': 'success'
+                        })
+                    except Exception as e:
+                        return jsonify({
+                            'status': 'error',
+                            'error_message': e
+                        })
         else:
             return jsonify({
                 'status': 'error',
@@ -85,6 +138,22 @@ def api():
                     'status': 'error',
                     'error_message': 'File ID: {0} does not exist'.format(file_id)
                 })
+            else:
+                try:
+                    delete_(file_id)
+                    return jsonify({
+                        'status': 'success'
+                    })
+                except Exception as e:
+                    return jsonify({
+                        'status': 'error',
+                        'error_message': e
+                    })
+        else:
+            return jsonify({
+                'status': 'error',
+                'error_message': 'The only operation allowed with the DELETE method is delete, you specified: {0}'.format(operation)
+            })
 
 if __name__ == "__main__":
     app.run()
