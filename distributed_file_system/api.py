256ef0925d11d89e2f34fb6dd4f750232e617da5
Mark Collier <markcollier@cs.uchicago.edu>
2017-12-02 00:18:42 +0000

    initial client library developed

--- distributed_file_system/api.py
+++ distributed_file_system/api.py
@@ -9,23 +9,27 @@ app = Flask(__name__)
 
 # open {r, w}, close, read, write
 
-def check_file_exists(file_id):
+def does_file_exist(file_id):
     pass
 
-@app.route("/", methods=['GET', 'POST', 'PUT'])
+@app.route("/", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def api():
     operation = request.args.get('operation')
     file_id = request.args.get('file_id')
     
     if request.method == 'POST':
         if operation == 'write':
-            pass
+            if not does_file_exist(file_id):
+                return jsonify({
+                    'status': 'error',
+                    'error_message': 'File ID: {0} does not exist'.format(file_id)
+                })
         else:
             return jsonify({
                 'status': 'error',
                 'error_message': 'The only operation allowed with the POST method is write, you specified: {0}'.format(operation)
             })
-    if request.method == 'PUT':
+    elif request.method == 'PUT':
         if operation == 'open':
             mode = request.args.get('mode')
             if not mode:
@@ -33,15 +37,36 @@ def api():
                     'status': 'error',
                     'error_message': 'You called the open operation, but did not specify a mode'
                 })
+            else:
+                if mode == 'read':
+                    if not does_file_exist(file_id):
+                        return jsonify({
+                            'status': 'error',
+                            'error_message': 'File ID: {0} does not exist'.format(file_id)
+                        })
+                    else:
+                        pass
+                elif mode == 'write':
+                    pass
         elif operation == 'close':
-            pass
+            if not does_file_exist(file_id):
+                return jsonify({
+                    'status': 'error',
+                    'error_message': 'File ID: {0} does not exist'.format(file_id)
+                })
         else:
             return jsonify({
                 'status': 'error',
                 'error_message': 'The only operations allowed with the PUT method are open, close, you specified: {0}'.format(operation)
             })
-    else:
-        elif operation == 'read':
+    elif request.method == 'GET':
+        if operation == 'read':
+            if not does_file_exist(file_id):
+                return jsonify({
+                    'status': 'error',
+                    'error_message': 'File ID: {0} does not exist'.format(file_id)
+                })
+
             num_bytes = request.args.get('num_bytes')
             if not num_bytes:
                 return jsonify({
@@ -53,6 +78,13 @@ def api():
                 'status': 'error',
                 'error_message': 'The only operation allowed with the GET method is read, you specified: {0}'.format(operation)
             })
+    elif request.method == 'DELETE':
+        if operation == 'delete':
+            if not does_file_exist(file_id):
+                return jsonify({
+                    'status': 'error',
+                    'error_message': 'File ID: {0} does not exist'.format(file_id)
+                })
 
 if __name__ == "__main__":
     app.run()
--- distributed_file_system/client.py
+++ distributed_file_system/client.py
@@ -5,7 +5,8 @@ class Client:
         self.server_ip = server_ip
 
     def open(self, file_id, mode):
-        r = requests.get(self.server_ip, params={
+        r = requests.put(self.server_ip, data={
+            'operation', 'open',
             'file_id': file_id,
             'mode': mode
         })
@@ -16,10 +17,48 @@ class Client:
             raise Exception(res.get('error_message'))
 
     def close(self, file_id):
-        pass
+        r = requests.put(self.server_ip, data={
+            'operation', 'close',
+            'file_id': file_id
+        })
+        res = r.json()
+        if res.get('status') == 'success':
+            return file_id
+        else:
+            raise Exception(res.get('error_message'))
 
     def read(self, file_id, num_bytes):
-        pass
+        r = requests.get(self.server_ip, params={
+            'operation', 'read',
+            'file_id': file_id,
+            'num_bytes': num_bytes
+        })
+        res = r.json()
+        if res.get('status') == 'success':
+            return res.get('bytes')
+        else:
+            raise Exception(res.get('error_message'))
 
     def write(self, file_id, bytes):
-        pass
+        r = requests.post(self.server_ip, data={
+            'operation', 'write',
+            'file_id': file_id,
+            'bytes': bytes
+        })
+        res = r.json()
+        if res.get('status') == 'success':
+            return file_id
+        else:
+            raise Exception(res.get('error_message'))
+
+    def delete(self, file_id):
+        r = requests.delete(self.server_ip, data={
+            'operation', 'delete',
+            'file_id': file_id
+        })
+        res = r.json()
+        if res.get('status') == 'success':
+            return file_id
+        else:
+            raise Exception(res.get('error_message'))
+
